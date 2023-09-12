8b0e9c53de22d1a73bf409f782f55bbe42750fee
cphenrique <88631495+cphenrique@users.noreply.github.com>
2023-09-12 09:08:00 -0300

    Add files via upload
    first commit

---
+++ api/schemas/__init__.py
@@ -0,0 +1,2 @@
+from schemas.carro import CarroSchema, CarroViewSchema, ListagemCarrosSchema, CarroBuscaSchema, CarroDelSchema, apresenta_carros, apresenta_carro
+from schemas.error import ErrorSchema

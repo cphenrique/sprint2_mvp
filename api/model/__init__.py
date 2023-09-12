641c971a7b32784facb9bdce1c2475db5313a127
cphenrique <costa.phenrique@gmail.com>
2023-09-12 10:22:44 -0300

    Conexão com o banco de dados postgres

--- api/model/__init__.py
+++ api/model/__init__.py
@@ -7,14 +7,8 @@ import os
 from model.base import Base
 from model.carro import Carro
 
-db_path = "database/"
-# Verifica se o diretorio não existe
-if not os.path.exists(db_path):
-   # então cria o diretorio
-   os.makedirs(db_path)
-
 # url de acesso ao banco (essa é uma url de acesso ao sqlite local)
-db_url = 'sqlite:///%s/db.sqlite3' % db_path
+db_url = 'postgresql://postgres:postgres@localhost:5432/revenda'
 
 # cria a engine de conexão com o banco
 engine = create_engine(db_url, echo=False)
